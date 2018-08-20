e2af0d0c96cd7c4cf7ef558e3400c3af21e1c133
kief <kmorris@thoughtworks.com>
2018-08-20 10:33:02 +0100

    Load spec from yaml file. With tests, probably too much, but they helped

--- lib/cloudspin/stack/definition.rb
+++ lib/cloudspin/stack/definition.rb
@@ -1,7 +1,40 @@
+require 'yaml'
 
 module Cloudspin
   module Stack
     class Definition
+
+      attr_reader :instance_parameter_names, :required_resource_names, :terraform_source_path
+
+      def initialize( terraform_source_path: '',
+                      instance_parameter_names: [],
+                      required_resource_names: []
+                    )
+        @terraform_source_path = terraform_source_path
+        @instance_parameter_names = instance_parameter_names
+        @required_resource_names = required_resource_names
+      end
+
+      def self.from_file(specfile)
+        spec_hash = YAML.load_file(specfile)
+        stack_spec = symbolize(spec_hash)
+        self.new(terraform_source_path: 'todo', **stack_spec)
+      end
+
+      private
+
+      def self.symbolize(obj)
+          return obj.inject({}){ |memo, (k,v)|
+            memo[k.to_sym] = symbolize(v)
+            memo
+          } if obj.is_a? Hash
+          return obj.inject([]) { |memo, v|
+            memo << symbolize(v)
+            memo
+          } if obj.is_a? Array
+          return obj
+      end
+
     end
   end
 end
---
+++ spec/cloudspin/stack_definition_spec.rb
@@ -0,0 +1,107 @@
+require 'tempfile'
+
+RSpec.describe 'Stack defined with defaults' do
+
+  let(:stack_definition) {
+    Cloudspin::Stack::Definition.new()
+  }
+
+  it 'has the expected terraform source path' do
+    expect(stack_definition.terraform_source_path).to eq('')
+  end
+
+  it 'has an empty list of instance parameter names' do
+    expect(stack_definition.instance_parameter_names).to be_empty
+  end
+
+  it 'has an empty list of required resource_names names' do
+    expect(stack_definition.required_resource_names).to be_empty
+  end
+
+end
+
+
+RSpec.describe 'Stack defined from code' do
+
+  let(:stack_definition) {
+    Cloudspin::Stack::Definition.new(
+      terraform_source_path: 'some/path',
+      instance_parameter_names: [ 'a', 'b'],
+      required_resource_names: [ 'x', 'y' ]
+    )
+  }
+
+  it 'has the expected terraform source path' do
+    expect(stack_definition.terraform_source_path).to eq('some/path')
+  end
+
+  it 'has the defined list of instance parameter names' do
+    expect(stack_definition.instance_parameter_names).to contain_exactly('a', 'b')
+  end
+
+  it 'has the defined list of required resource_names names' do
+    expect(stack_definition.required_resource_names).to contain_exactly('x', 'y')
+  end
+
+end
+
+
+RSpec.describe 'Stack defined from code with splat' do
+
+  let(:stack_definition) {
+    spec_hash = {
+      :instance_parameter_names => [ 'a', 'b'],
+      :required_resource_names => [ 'x', 'y' ]
+    }
+    Cloudspin::Stack::Definition.new(
+      terraform_source_path: 'some/path', **spec_hash
+    )
+  }
+
+  it 'has the expected terraform source path' do
+    expect(stack_definition.terraform_source_path).to eq('some/path')
+  end
+
+  it 'has the instance parameter names passed from code' do
+    expect(stack_definition.instance_parameter_names).to contain_exactly('a', 'b')
+  end
+
+  it 'has the required resource names passed from code' do
+    expect(stack_definition.required_resource_names).to contain_exactly('x', 'y')
+  end
+
+end
+
+
+RSpec.describe 'Stack defined from yaml spec file' do
+
+  let(:yaml_file) {
+    tmp = Tempfile.new('stack_definition_spec.yaml')
+    tmp.write(<<~YAML_FILE
+      ---
+      instance_parameter_names:
+      - foo
+      - bar
+      required_resource_names:
+      - thing_one
+      - thing_two
+      YAML_FILE
+    )
+    tmp.close
+    tmp.path
+  }
+
+  let(:stack_definition) {
+    Cloudspin::Stack::Definition.from_file(yaml_file)
+  }
+
+  it 'has the instance parameter names defined in the yaml file' do
+    expect(stack_definition.instance_parameter_names).to contain_exactly('foo', 'bar')
+  end
+
+  it 'has the required resource names defined in the yaml file' do
+    expect(stack_definition.required_resource_names).to contain_exactly('thing_one', 'thing_two')
+  end
+
+end
+
