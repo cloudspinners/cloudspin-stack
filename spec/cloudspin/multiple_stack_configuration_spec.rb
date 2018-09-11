d26355d7c69013ef32fd0f7815f9084c61f14a62
kief <kmorris@thoughtworks.com>
2018-09-11 14:33:01 +0100

    Add a fairly useless test

--- spec/cloudspin/multiple_stack_configuration_spec.rb
+++ spec/cloudspin/multiple_stack_configuration_spec.rb
@@ -2,7 +2,39 @@
 RSpec.describe 'Stack::Instance' do
 
   let(:stack_definition) {
-    Cloudspin::Stack::Definition.new(source_path: source_path, stack_name: 'my_stack')
+    Cloudspin::Stack::Definition.new(source_path: '/dev/null/src', stack_name: 'my_stack')
   }
+
+  let(:instance_configuration) {
+    Cloudspin::Stack::InstanceConfiguration.new(stack_definition)
+  }
+
+  let(:stack_instance_one) {
+    Cloudspin::Stack::Instance.new(
+      id: 'stack_one',
+      stack_definition: stack_definition,
+      working_folder: '/dev/null/work/stack_one',
+      statefile_folder: '/dev/null/state/stack_one',
+      configuration: instance_configuration
+    )
+  }
+
+  let(:stack_instance_two) {
+    Cloudspin::Stack::Instance.new(
+      id: 'stack_two',
+      stack_definition: stack_definition,
+      working_folder: '/dev/null/work/stack_two',
+      statefile_folder: '/dev/null/state/stack_two',
+      configuration: instance_configuration
+    )
+  }
+
+  describe 'two instances' do
+    it 'do not clash' do
+      expect(stack_instance_one.id).to eq('stack_one')
+      expect(stack_instance_two.id).to eq('stack_two')
+    end
+
+  end
 
 end
