c88ebc31efbde08534c5b6a3ef9991e35a184dd8
kief <kmorris@thoughtworks.com>
2018-09-27 13:04:23 +0100

    Add configuration option to tag code in github.

--- lib/cloudspin/stack/definition.rb
+++ lib/cloudspin/stack/definition.rb
@@ -8,10 +8,11 @@ module Cloudspin
       attr_reader :version
       attr_reader :source_path
 
-      def initialize(source_path:, stack_name:, stack_version: '0')
+      def initialize(source_path:, stack_name:, stack_version: '0', github_tag: false)
         @source_path = source_path
         @name = stack_name
         @version = stack_version
+        @github_tag = !github_tag.nil? && (github_tag || /^(true|yes|1)$/i.match(github_tag))
       end
 
       def self.from_file(specfile)
@@ -21,12 +22,17 @@ module Cloudspin
         self.new(
           source_path: source_path,
           stack_name: spec_hash.dig('stack', 'name'),
-          stack_version: spec_hash.dig('stack', 'version')
+          stack_version: spec_hash.dig('stack', 'version'),
+          github_tag: spec_hash.dig('stack', 'github_tag')
         )
       end
 
       def self.from_folder(definition_folder)
         from_file("#{definition_folder}/stack-definition.yaml")
+      end
+
+      def github_tag?
+        @github_tag
       end
 
     end
